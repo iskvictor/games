7b4ee5943de077d61a9606b058f0f669ab18391b
iskvictor <victor.isk83@gmail.com>
2019-12-12 23:19:22 +0200

    add control.py

---
+++ snake/control.py
@@ -0,0 +1,22 @@
+import pygame
+from pygame.locals import *
+
+class Control():
+    def __init__(self):
+        self.play = True
+        self.direction = "RIGTH"
+
+
+    def control(self):
+        for i in pygame.event.get():
+            if i.type == QUIT:
+                self.play = False
+            elif i.type == KEYDOWN:
+                if i.key == K_RIGHT:
+                    self.direction = "RIGTH"
+                elif i.key == K_LEFT:
+                    self.direction = "LEFT"
+                elif i.key == K_UP:
+                    self.direction = "UP"
+                elif i.key == K_DOWN:
+                    self.direction == "DOWN"
